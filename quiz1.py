aa4a44196ff2fde0dfe5a4dc6c022370e2ac8d1d
Nikhil23rai <nrai29832@gmail.com>
2024-11-21 23:34:40 +0530

    Update quiz1.py

--- quiz1.py
+++ quiz1.py
@@ -36,50 +36,50 @@ if user1 in users:
                 print("wrong password")
                 continue
 
-# filename = 'questions.txt'
-# def loading(filename):
-#     questions = []
+filename = 'questions.txt'
+def loading(filename):
+    questions = []
     
-#     try:   
-#         with open(filename, 'r') as file:
-#             lines = file.readlines()
-#             for i in range(0, len(lines)-1, 2):
-#                 question = lines[i].strip()
-#                 answer = lines[i + 1].strip()
-#                 questions.append((question, answer))
-#     except FileNotFoundError:
-#         print(f"Error: The file '{filename}' was not found.")
-#         return None
+    try:   
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            for i in range(0, len(lines)-1, 2):
+                question = lines[i].strip()
+                answer = lines[i + 1].strip()
+                questions.append((question, answer))
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+        return None
 
-#     return questions
+    return questions
 
-# def askingquestion(question, correct_ans):
-#     print(question)
-#     user_answer = input("Your answer: ").strip().lower()
-#     if user_answer == correct_ans.lower():
-#         print("Correct!")
-#         return True
-#     else:
-#         print(f"Wrong! The correct answer is: {correct_ans}")
-#         return False
-
-
-# def play(questions):
-#     score = 0
-#     for question, correct_ans in questions:
-#         if askingquestion(question, correct_ans):
-#             score += 1
-#     print(f"\nGame Over! Your score: {score}/{len(questions)}") 
+def askingquestion(question, correct_ans):
+    print(question)
+    user_answer = input("Your answer: ").strip().lower()
+    if user_answer == correct_ans.lower():
+        print("Correct!")
+        return True
+    else:
+        print(f"Wrong! The correct answer is: {correct_ans}")
+        return False
 
 
-# def main():
-#     questions = loading(filename)
+def play(questions):
+    score = 0
+    for question, correct_ans in questions:
+        if askingquestion(question, correct_ans):
+            score += 1
+    print(f"\nGame Over! Your score: {score}/{len(questions)}") 
 
-#     if not questions: 
-#         print("No questions available. Exiting the quiz.")
-#         return
+
+def main():
+    questions = loading(filename)
+
+    if not questions: 
+        print("No questions available. Exiting the quiz.")
+        return
     
-#     print("Welcome to the Quiz!\n")
-#     play(questions)
+    print("Welcome to the Quiz!\n")
+    play(questions)
 
-# main()
+main()
