3b5f81038f5580b38e1b81807a3bd208af713d8d
Nikhil23rai <nrai29832@gmail.com>
2024-11-22 00:05:56 +0530

    Update h.py

--- h.py
+++ h.py
@@ -1,25 +1,4 @@
-# arr1=[2,3,4,5,8]
-# arr2=[]
-# maxi=max(arr1)
-# for i in range(len(arr1)):
-#      arr2.append(arr1[i]+maxi)
-#      arr2[i]*=maxi
-# print(arr2)
 
-# for i in range(len(arr1)):
-#     arr2.append(arr1[i]+max)
-# #
-
-#         arr2[i]*=max
-# print(arr2)
-# arr=[2,3,4,58]
-# maxi=arr[0]
-# for i in arr:
-#     if i>=maxi:
-#         maxi=i
-# print(maxi)
-# print('Q1: Which one is the fruit in the given options?')
-# print("Q2: City of lake ?")
 que={'Q1:Which one is the fruit in the given options?':"a",
     "Q2 :City of lake ?":'c',
     'Q3:which one is animal ?':'b',
